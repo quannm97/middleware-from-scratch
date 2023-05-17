1334092cb722a31c5e9552c23db93da0aa0593ef
quannm97 <quan.nm.dever@gmail.com>
2023-05-17 20:55:26 +0700

    fix the param of func.js

--- dispatchFunc.js
+++ dispatchFunc.js
@@ -7,9 +7,16 @@
 const store = configureStore()
 
 // store.dispatch(() => {})   error if call like this
+
 /**
  * Here is the idea, when work with API we want to Call API
  * API usually return a Promise
  * When the Promise is resolve => dispatch()
  * if the Promise is reject, we dispatch another function
+*/
+store.dispatch(() => {
+    store.dispatch({type:'bugsReceived', bugs: [1, 2, 3]})
+})
+/**
+ * There are 2 actions, the first one is the aciton we dispatch, the second one is the action that contains the result of our API call
  */
